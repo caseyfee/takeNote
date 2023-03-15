76d087e7a50781f464f79dec88f1d572bd83af7f
caseyfee <caseyfee3@gmail.com>
2023-03-15 10:45:17 -0700

    pre BCS - cannotfind module error

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(clog);
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api', api);
 
 app.use(express.static('public'));
